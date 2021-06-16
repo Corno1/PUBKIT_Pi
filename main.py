5957a61f41167a35fee1a98eee8f2901be9b15b1
Mario <mario.rosshirt@gmail.com>
2021-06-16 13:46:30 +0200

    added database connection and insert statement for timestamp and temperature

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import sense_hat, random, time
 from sense_hat import SenseHat
 sense = SenseHat()
+from datetime import datetime
+import mysql.connector
+
+db = mysql.connector.connect(
+  host="database-1.c2ggfvafdmjq.us-east-1.rds.amazonaws.com",
+  user="admin",
+  password="START123!",
+  database="ITINFRA"
+)
+cursor = db.cursor()
 
 while True:
-  t = sense.get_temperature()
-
-  print(t)
-  time.sleep(1)
+    temp = sense.get_temperature()
+    timestamp = datetime.now()
+    statement = "INSERT INTO Data (Timestamp, Temp) VALUES ({}, {})".format(timestamp, temp)
+    cursor.execute(statement)
+    print(temp)
+    time.sleep(1)
